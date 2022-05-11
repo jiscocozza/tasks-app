1836637b53ec6ba3acebbb3b522884a27e0aae8a
jiscocozza <jiscocozza@gmail.com>
2022-05-11 10:16:16 -0300

    Added handleSubmit on form

--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -2,33 +2,33 @@ import React, { useState } from "react";
 import Task from "./Task"
 import { v4 as uuidv4 } from 'uuid';
 
-const tasksMock = [
+/* const tasksMock = [
   { id: 1, text: "Clean my bedroom", done: false },
   { id: 2, text: "Finish homework", done: false },
   { id: 3, text: "Bath dog", done: false },
   { id: 4, text: "Bath cat", done: false }
-];
+]; */
 
 function TasksList() {
 
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-  const addTask = (event)=> {
-    /*event.preventDefault();*/
+  const addTask = ()=> {
     const newTask = {
       id: uuidv4(),
       text: inputValue,
       done: false
     }
 
-    console.log(newTask);
     if (newTask.text.trim()) {
       newTask.text = newTask.text.trim();
       setTasks([newTask, ...tasks]);
     } else {
       alert("Please, add text to your new task.")
     }
+    setInputValue("");
+
   };
 
   const deleteTask = id => {
@@ -49,10 +49,15 @@ function TasksList() {
     setInputValue(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTask();
+  }
+
   return (
     <>
-      <form>
-        <input type="text" onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input value={inputValue} type="text" onChange={handleChange} />
         <button type="button" onClick={addTask}>Add task</button>
       </form>
   
